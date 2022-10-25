44ee4891e5dced7248af800f51647edb487fbc37
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-26 08:25:43 +0900

    basic provider

--- example1/lib/main.dart
+++ example1/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,13 +20,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final dateTimeProvider = Provider<DateTime>((ref) => DateTime.now());
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(dateTimeProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Example 1')),
+      body: Center(child: Text(date.toIso8601String())),
     );
   }
 }
