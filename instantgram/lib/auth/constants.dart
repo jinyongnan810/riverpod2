e228624f7271bb16d77d4009ec3fb27370174dc8
Kin Yuunan <yuunan.kin@gmail.com>
2022-11-07 08:44:08 +0900

    auth day 1

---
+++ instantgram/lib/auth/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  // https://cloud.google.com/identity-platform/docs/link-accounts#handling_the_account-exists-with-different-credential_error
+  static const accountExistsWithDifferentCredential =
+      'account-exists-with-different-credential';
+  static const googleOrigin = 'google.com';
+  static const emailScope = 'email';
+  const Constants._();
+}
